ce5bd2769161ca124db98519201876eb4f1a0708
Elvann <elvann.abendroth@gmail.com>
2023-07-10 15:54:10 +0300

    🌈 dashboard profile picture & username

--- src/app/(index)/dashboard/page.tsx
+++ src/app/(index)/dashboard/page.tsx
@@ -2,14 +2,28 @@ import Image from 'next/image'
 import data from '@/lib/data'
 import AddLinkForm from '@/components/AddLinkForm'
 import LinkCardEdit from '@/components/LinkCardEdit'
+import Link from 'next/link'
 
 export default function DashboardPage() {
   const { username, imageUrl, links } = data
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-col justify-center items-center">
-        <Image className="rounded-full shadow-md" src={imageUrl} alt="profile picture" width={120} height={120} />
-        <p className="typo-p font-display font-semibold text-lg text">@{username}</p>
+        <Link href="/profile">
+          <Image
+            className="rounded-full shadow-md hover:opacity-90"
+            src={imageUrl}
+            alt="profile picture"
+            width={120}
+            height={120}
+          />
+        </Link>
+        <Link
+          href="/profile"
+          className="typo-p font-display font-semibold text-lg text flex gap-2 items-center hover:text-foreground hover:text-foreground/80"
+        >
+          @{username}
+        </Link>
       </div>
       <AddLinkForm />
       {links.map(link => (

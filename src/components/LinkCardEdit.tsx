30717bf1ffc90dfff1dc967121f9e37ce0151a75
Elvann <elvann.abendroth@gmail.com>
2023-07-12 20:14:41 +0300

    🌈 Overflow fix

--- src/components/LinkCardEdit.tsx
+++ src/components/LinkCardEdit.tsx
@@ -46,33 +46,25 @@ export default function LinkCardEdit({ link }: LinkCardEditProps) {
           <Icons.check size={20} />
           Save
         </Button>
-        {/* <Button onClick={e => setIsEditMode(!isEditMode)} variant="danger" size="sm">
-          <Icons.trash size={20} />
-          Delete Link
-        </Button> */}
       </form>
     )
 
   return (
     <div className="bg-input/60 shadow-lg shadow-muted/30 flex gap-4 rounded-lg p-8 items-start ">
-      <div className="flex flex-col gap-4 flex-grow">
-        <p className="typo-h4 flex gap-2 items-center cursor-pointer justify-between">
+      <div className="flex flex-col gap-4 flex-grow max-w-full">
+        <p className="typo-h4 flex gap-2 items-center justify-between">
           {title}
           <Icons.pen onClick={e => setIsEditMode(!isEditMode)} className="cursor-pointer" size={20} />
         </p>
+        <p className="typo-p text-muted break-words pr-8">{href}</p>
 
-        <div className="flex justify-between items-end">
+        <div className="flex flex-row justify-between items-end">
           <div className="flex flex-col gap-4">
-            <p className="text-muted cursor-pointer">{href}</p>
             <Switch className="data-[state=unchecked]:bg-muted/20" checked={isActive} />
           </div>
           <Icons.trash className="cursor-pointer" size={20} />
         </div>
       </div>
-      {/* <div className="flex flex-col h-full place-center items-center justify-center gap-3 content-center">
-        <Icons.pen onClick={e => setIsEditMode(!isEditMode)} className="cursor-pointer" size={20} />
-        <Icons.trash className="cursor-pointer" size={20} />
-      </div> */}
     </div>
   )
 }

b7c408811b00d7bb28b99e7515d62e8b2b27000c
Elvann <elvann.abendroth@gmail.com>
2023-07-20 13:42:55 +0300

    🔨 User Profile & Dashboard

--- src/lib/data.server.ts
+++ src/lib/data.server.ts
@@ -12,39 +12,29 @@ export async function getSessionUser() {
 
 export async function getUserById(id: string) {
   try {
-    const user = (await User.findById(id).lean()) as UserDocument
-    if (!user) throw new Error("Couldn't find user")
-    //There's a warning in the console when passing down links because they have the _id property, but it seems to work anyways
-    return user as UserData
+    const res = await fetch(`${process.env.VERCEL_URL}/api/users/${id}`)
+    const user = await res.json()
+    return user
   } catch (error) {
-    console.log(error)
-    throw new Error(`Error while querying data: ${error}`)
+    throw new Error('There was an error fetching this user.')
   }
-
-  // let safeUser = {
-  //   ...user,
-  //   id: user?._id.toString(),
-  // }
-  // delete safeUser.password
-  // delete safeUser._id
-  // delete safeUser.__v
 }
 
-export async function getUserByUsername(username: string) {
-  try {
-    const user = (await await User.findOne({ username: username }).lean()) as UserDocument
-    if (!user) notFound()
-    //There's a warning in the console when passing down links because they have the _id property, but it seems to work anyways
-    return user as UserData
-  } catch (error) {
-    throw new Error(`${error}`)
-  }
+// export async function getUserByUsername(username: string) {
+//   try {
+//     const user = (await await User.findOne({ username: username }).lean()) as UserDocument
+//     if (!user) notFound()
+//     //There's a warning in the console when passing down links because they have the _id property, but it seems to work anyways
+//     return user as UserData
+//   } catch (error) {
+//     throw new Error(`${error}`)
+//   }
 
-  // let safeUser = {
-  //   ...user,
-  //   id: user?._id.toString(),
-  // }
-  // delete safeUser.password
-  // delete safeUser._id
-  // delete safeUser.__v
-}
+// let safeUser = {
+//   ...user,
+//   id: user?._id.toString(),
+// }
+// delete safeUser.password
+// delete safeUser._id
+// delete safeUser.__v
+// }

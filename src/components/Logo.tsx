4436414db2081bd3e5bebaf744ab18117602d48a
Elvann <elvann.abendroth@gmail.com>
2024-06-03 09:41:21 +0300

    🔨

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,38 +1,40 @@
 import Link from 'next/link'
-import React, { FC, useContext } from 'react'
+import React, { FC, HTMLAttributes } from 'react'
 import { Icons } from './Icons'
 import { siteConfig } from '@/lib/config'
-import { SessionContext } from 'next-auth/react'
+
 import { cn } from '@/lib/utils'
 
-interface LogoProps extends React.ForwardRefExoticComponent<any> {
+interface LogoProps {
   variant?: 'default' | 'icon'
-  className: string
+  className?: string
 }
 
 export const Logo: FC<LogoProps> = ({ variant = 'default', className, ...props }) => {
-  // const session = useContext(SessionContext)
-
   if (variant === 'icon')
     return (
+      <div>
+        <Link
+          className={cn(
+            'text-foreground hover:text-foreground-hover font-semibold text-2xl font-display flex gap-2 place-items-center',
+            className
+          )}
+          href="/"
+          {...props}
+        >
+          <Icons.logo strokeWidth={3} />
+        </Link>
+      </div>
+    )
+  return (
+    <div>
       <Link
-        className={cn(
-          'text-foreground hover:text-foreground-hover font-semibold text-2xl font-display flex gap-2 place-items-center',
-          className
-        )}
+        className="font-semibold text-2xl font-display hover:text-gray-800 flex gap-2 place-items-center"
         href="/"
         {...props}
       >
-        <Icons.logo strokeWidth={3} />
+        <Icons.logo strokeWidth={3} /> <span>{siteConfig.name}</span>
       </Link>
-    )
-  return (
-    <Link
-      className="font-semibold text-2xl font-display hover:text-gray-800 flex gap-2 place-items-center"
-      href="/"
-      {...props}
-    >
-      <Icons.logo strokeWidth={3} /> <span>{siteConfig.name}</span>
-    </Link>
+    </div>
   )
 }
